c478d73c8fa789097d887175fe9e0861273a41e2
kubousky <kparcheta@gmail.com>
2019-11-29 20:08:42 +0100

    fourth commit

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import datetime
 import matplotlib.pyplot as plt
 import sys
 
-""" Tarea 1: 
-Define completamente una estructura de datos (registro) ADECUADA
-para guardar la información de UNA línea del fichero para poder
-trabajar ADECUADAMENTE con ella. 
-La estructura de datos (registro) se llamará InfoTerremoto.
-Deberá incluir NECESARIAMENTE un campo de tipo Fecha 
-y un campo de tipo Hora para guardar la fecha y hora del terremoto. """
 
 class InfoTerremoto:
     def __init__(self, Year, Month, Day, Hora, Mag, Lat, Lon, Depth_km, Region, IRIS_ID, Timestamp):
@@ -35,24 +28,12 @@ class InfoTerremoto:
         print("IRIS_ID: " + str(self.IRIS_ID))
         print("Region: " + self.Region)
 
-""" Tarea 2:
-Define una función AnalizaLinea que permita analizar una línea del fichero y rellenar 
-y devolver UNA estructura de datos InfoTerremoto con la información de 
-UNA línea del fichero pasada como parámetro.
-Comprueba el correcto funcionamiento de la función creando un pequeño programa principal 
-que llame a la función con una línea del fichero (str) como parámetro 
-y que, a continuación, muestre la información de los diferentes campos de InfoTerremoto. """
+
 
 def AnalizaLinea(linea):
     DatosLinea = linea.split(";")
     return InfoTerremoto(DatosLinea[0],DatosLinea[1],DatosLinea[2],DatosLinea[3],DatosLinea[4],DatosLinea[5],DatosLinea[6],DatosLinea[7],DatosLinea[8],DatosLinea[9],DatosLinea[10])
 
-""" Tarea 3: 
-Define una función LeerFichero que permita leer un fichero linea 
-a linea con la información de los terremotos guardada en el fichero. 
-A la función se le pasará el NOMBRE del fichero y devolverá una lista de elementos de tipo InfoTerremoto. 
-Además, la función devolverá verdadero si se ha podido abrir el fichero 
-y falso si no se ha podido abrir el fichero. """
 
 def LeerFichero(fichero):   
     try:
@@ -93,16 +74,12 @@ def MaxTerremoto(registros):
     for r in registros:
         if (r.Fecha >= fecha_inicio) and (r.Fecha <= fecha_fin): 
             if maxRegister is None:
-                maxRegister = r  # primer terremoto r.Mag , maxRegister.Mag
+                maxRegister = r 
             else:
                 if (maxRegister.Mag < r.Mag):
                     maxRegister = r
     return maxRegister
 
-""" Tarea 7: 
-Realiza una función ObtenerLista que nos devuelva una lista con las magnitudes de los terremotos 
-producidos en un area concreta del planeta delimitada por una latitud mínima 
-y una latitud máxima y por una longitud mínima y una longitud máxima. """
 
 def ObtenerLista(registros):
     min_lat = float(input("Introduce un minima latitud: "))
@@ -115,8 +92,7 @@ def ObtenerLista(registros):
             listaMag.append(r.Mag)
     return listaMag
 
-""" Vamos a crear una funcion que nos devuelve objetos de terremotos producidos 
-en una area concreta de la planeta """
+
 
 def ObtenerListaInfo(registros):
     min_lat = float(input("Introduce un minima latitud: "))
@@ -130,26 +106,15 @@ def ObtenerListaInfo(registros):
     return terremotos
 
 
-""" Tarea 8: 
-Realizar una función ObtenerRegiones que nos devuelve en 
-una lista todas las regiones que aparecen en la lista de InfoTerremoto. """
 
 def ObtenerRegiones(registros):
     Regiones = []
     for i in registros:
-        if (i.Region not in Regiones): #comprobamos si region ya esta en lista Regiones, si no está añadimos 
+        if (i.Region not in Regiones):
             Regiones.append(i.Region)
     return Regiones
 
-""" Tarea 9: 
-Realizar una función TerremotosRegion que nos diga 
-la magnitud media de los terremotos producidos en una región concreta entre dos fechas. 
-Se pasará la región y las fechas como parámetros y nos devolverá 
-el valor de la magnitud media de los terremotos.
-Si la región no existiera devolverá un '-2'. 
-Si no se produjeron terremotos en la región entre las fechas pasadas como parámetros, 
-devolverá un '-1'.
- """
+
 
 def TerremotosRegion(registros):
     region = input("Introduce una region sobre la que quieres recibir la información: ")
@@ -164,7 +129,7 @@ def TerremotosRegion(registros):
     for r in registros:
         if (r.Region == region):
             if (r.Fecha >= fecha_inicio) and (r.Fecha <= fecha_fin): 
-                sumaMag += r.Mag # registros es una lista de objetos, 'r' es cada objeto, magnitud es 'r.Mag'
+                sumaMag += r.Mag 
                 numero_terremotos += 1
 
     if numero_terremotos > 0:
@@ -173,10 +138,6 @@ def TerremotosRegion(registros):
     else:
         return -1
 
-""" Guardar en un fichero la información de los terremotos entre dos fechas concretas. 
-En este fichero SOLO guardaremos la fecha, la magnitud, la latitud y longitud donde se produjo el terremoto 
-y la profundidad en kilómetros. 
-El orden en las fechas guardadas seguirá siendo desde la fecha más antigua hasta la fecha más actual. """
 
 
 def GuardarFichero(registros):
@@ -195,9 +156,7 @@ def GuardarFichero(registros):
     print("Su fichero {} se guardó correctamente.".format(nombre_fichero))
 
     
-'''
-Imprime el menu por pantalla
-'''
+
 def print_menu():
 	print("1. Obtener el primer máximo terremoto producido entre dos fechas concretas.")
 	print("2. Obtener la magnitud media de los terremotos producidos en una determinada región del planeta entre dos fechas concretas.")
@@ -206,12 +165,6 @@ def print_menu():
 	print("5. Salir del programa.")
 
 
-
-""" Tarea 4: 
-Una vez realizadas y verificadas las funciones, 
-implementa un programa completo PrFinal03.py que pida el nombre de un fichero 
-y cargue la información del fichero en una lista de elementos de tipo InfoTerremoto 
-(no tiene que mostrar por pantalla la información cargada). """
 
 def main():
 
